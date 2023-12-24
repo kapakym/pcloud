70e47348535f52ffa7e65d664467d06144331914
Alex P <mycop14@mail.ru>
2023-12-24 15:31:46 +0300

    add transitions

--- client/pclient/src/shared/ui/Button/ui/Button.tsx
+++ client/pclient/src/shared/ui/Button/ui/Button.tsx
@@ -2,7 +2,7 @@ import React, {FC, ButtonHTMLAttributes} from 'react';
 
 const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props, context) => {
     return (
-        <button className='border-[1px] border-black p-2 rounded-xl shadow-xl hover:bg-black hover:text-white' {...props}>
+        <button className='border-[1px] border-black p-2 rounded-xl shadow-xl hover:bg-black hover:text-white transition-all duration-500 transform active:scale-x-75 transition-transform mx-5 flex' {...props}>
             {props.children}
         </button>
     );
